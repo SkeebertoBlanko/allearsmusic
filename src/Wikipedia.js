c9824d04511b2fd5e24fddc6722397d96b1ad0e5
Schober-LBS <andreas.schober@lbs4.salzburg.at>
2019-10-11 15:43:54 +0200

    x

--- src/Wikipedia.js
+++ src/Wikipedia.js
@@ -1,18 +1,20 @@
 import React, { useState, useContext } from "react";
-import "./App.css";
-import { SearchContext } from "./store/Store";
+import { SearchContext } from "../store/Store";
+import "../App.css";
 
-export function Wikipedia(props) {
-  const [query, setQuery] = useContext(SearchContext);
+function Wikipedia(props) {
+  const [query] = useContext(SearchContext);
+  const [WikiSearchTerms, setWikiSearchTerms] = useState(query); // useState("query");
   const [wikiSearchReturnValues, setwikiSearchReturnValues] = useState([]);
-  const [WikiSearchTerms, setWikiSearchTerms] = useState(query);
 
-    const useWikiSearchEngine = e => {
+  const useWikiSearchEngine = e => {
+    e.preventDefault();
+
     setwikiSearchReturnValues([]);
 
-    let url = "https:en.wikipedia.org/w/api.php";
+    var url = "https:en.wikipedia.org/w/api.php";
 
-    let params = {
+    var params = {
       action: "query",
       list: "search",
       srsearch: WikiSearchTerms,
@@ -29,14 +31,16 @@ export function Wikipedia(props) {
         return response.json();
       })
       .then(function(response) {
+        let temp = [];
         for (var key in response.query.search) {
-          setwikiSearchReturnValues({
+          temp.push({
             queryResultPageFullURL: "no link",
             queryResultPageID: response.query.search[key].pageid,
             queryResultPageTitle: response.query.search[key].title,
             queryResultPageSnippet: response.query.search[key].snippet
           });
         }
+        setwikiSearchReturnValues(temp); // wikiSearchReturnValues(temp); --> not a function
       })
       .then(function(response) {
         for (var key2 in wikiSearchReturnValues) {
@@ -51,8 +55,9 @@ export function Wikipedia(props) {
             .then(function(response) {
               page.queryResultPageFullURL =
                 response.query.pages[pageID].fullurl;
+
+              /*  page.queryResultPageFullUR.forceUpdate(); // pointerToThis.forceUpdate(); */
             });
-            
         }
       });
   };
@@ -61,10 +66,12 @@ export function Wikipedia(props) {
     setWikiSearchTerms(query);
   };
 
+  // ----
+
   let WikiSearchResults = [];
 
   for (var key3 in wikiSearchReturnValues) {
-    setWikiSearchTerms(
+    WikiSearchResults.push(
       <div
         className="searchResultDiv border-2 border-black m-2 bg-gray-300 rounded"
         key={key3}
